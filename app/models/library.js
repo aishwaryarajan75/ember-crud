3b235d0feeb41a0abf0a8c53bd7818136f7e9daf
aishwaryarajan75 <aishwaryarajan75@gmail.com>
2017-06-17 15:08:44 -0500

    setup

---
+++ app/models/library.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+import Ember from 'ember';
+
+export default DS.Model.extend({
+
+  name: DS.attr('string'),
+  address: DS.attr('string'),
+  phone: DS.attr('string'),
+
+  isValid: Ember.computed.notEmpty('name')
+});
---
+++ app/router.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('about');
+  this.route('contact');
+
+  this.route('admin', function() {
+    this.route('invitations');
+  });
+  
+  this.route('libraries', function() {
+    this.route('new');
+	this.route('edit', { path: '/:library_id/edit' });
+  });
+});
+
+export default Router;
